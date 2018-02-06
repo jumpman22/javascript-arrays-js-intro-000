b85a60aa736e8be346ce14ec4b9b8431ae044ced
Mila J <icecold240@gmail.com>
2018-02-06 08:15:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,18 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
-
   return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
